4a2f3da837d02269438993479d2373c783fee773
unknown <github_cloudlabsuser_1245@cloudlabsaiuser.com>
2025-02-18 21:09:22 +0000

    New features for search

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,21 +9,35 @@ public class UserController : Controller
     public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
 
         // GET: User
-        public ActionResult Index()
+        public ActionResult Index(string searchName)
         {
             // Implement the Index method here
+            var users = userlist.AsQueryable();
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                users = users.Where(u => u.Name.Contains(searchName, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return View(users.ToList());
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
         }
 
         // POST: User/Create
@@ -31,6 +45,16 @@ public class UserController : Controller
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            try
+            {
+                user.Id = userlist.Count > 0 ? userlist.Max(u => u.Id) + 1 : 1;
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: User/Edit/5
@@ -38,6 +62,12 @@ public class UserController : Controller
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
 
         // POST: User/Edit/5
@@ -49,12 +79,34 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            try
+            {
+                var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                if (existingUser == null)
+                {
+                    return RedirectToAction("Error");
+                }
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return RedirectToAction("Error");
+            }
+            return View(user);
         }
 
         // POST: User/Delete/5
@@ -62,5 +114,19 @@ public class UserController : Controller
         public ActionResult Delete(int id, IFormCollection collection)
         {
             // Implement the Delete method (POST) here
+            try
+            {
+                var user = userlist.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    return RedirectToAction("Error");
+                }
+                userlist.Remove(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 }
